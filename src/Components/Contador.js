05daa968129fa68a00d0f084ccec5fe2f876ad6e
lorddorn <pedrotelles@gec.inatel.br>
2017-07-06 21:37:20 -0300

    added components app and contador plus updated index.html

---
+++ src/Components/Contador.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Contador extends React.Component{
+
+    render(){
+        return (
+            <h1></h1>
+            <div>
+                <button>-</button>
+                <button>+</button>
+            </div>
+        );
+    }
+    
+}
